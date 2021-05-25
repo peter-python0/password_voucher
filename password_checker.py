35a9fc2d1e51bd1b0b7e177c6f8e0258a41a0a31
peter-python0 <83729250+peter-python0@users.noreply.github.com>
2021-05-25 09:38:30 +0100

    Add files via upload

---
+++ password_checker.py
@@ -0,0 +1,45 @@
+import random
+import string
+import time
+
+class password_check:
+    """Program to check password & release voucher."""
+
+    def __init__(self, business):
+        self.business = business.lower()
+        self.count = 0
+        self.voucher = ''
+        self.usr = ''
+        self.passwords = []
+        self.unlocked = False
+
+    # not needed in final version
+    # def describe_business(self):
+    #     return f"The business type is: {self.business}."
+
+    def input_password(self):
+        self.usr = input("Type password: ").lower()
+        self.passwords = ['columbo', 'poirot', 'marple']
+        return p1.check_password()
+
+    def check_password(self):
+        if self.usr in self.passwords:
+            self.unlocked = True
+            return p1.release_voucher()
+        else:
+            self.unlocked = False
+            self.count += 1
+            print(self.count)
+            if self.count > 5:
+                print("Too many incorrect entries: wait 10 seconds.")
+                time.sleep(10)
+                return p1.input_password()
+            else:
+                return p1.input_password()
+
+    def release_voucher(self):
+            return ''.join(random.choices(string.ascii_lowercase + string.digits, k=16))
+
+p1 = password_check('cinema') # create instance of class
+# print(p1.describe_business()) # print to show that functions are returning
+print(p1.input_password()) # functions within functions, so this is only necessary command
